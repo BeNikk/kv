fc73fba4d3ad4a461835e506de14d404a43efe63
BeNikk <bhattnik442@gmail.com>
2026-04-28 17:05:41 +0530

    feat: add command and log entry types for Raft implementation

---
+++ src/raft/log.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Command {
+    Set { key: String, value: String },
+    Delete { key: String },
+    NoOp,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct LogEntry {
+    pub term:    u64, // era of leadership
+    pub index:   u64, // position of the command
+    pub command: Command, // command which will run
+}
--- src/raft/state.rs
+++ src/raft/state.rs
@@ -5,4 +5,5 @@ pub enum NodeRole {
     Follower,
     Candidate,
     Leader,
-}
+} // A Node can be either a Leader, or a Candidate to be a leader, or a Follower. 
+
