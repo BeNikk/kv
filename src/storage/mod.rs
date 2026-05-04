7c8435fb87e4de3ba0b93968435cfe9075e31ed1
BeNikk <bhattnik442@gmail.com>
2026-05-05 00:06:50 +0530

    feat: implement KvStore struct with apply and get methods for key-value storage operations

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,1 +1,26 @@
+use crate::raft::log::Command;
+use std::collections::HashMap;
 
+#[derive(Debug, Default)]
+pub struct KvStore {
+    data: HashMap<String, String>,
+    pub last_applied: u64,
+}
+
+impl KvStore {
+    pub fn apply(&mut self, index: u64, command: &Command) {
+        match command {
+            Command::Set { key, value } => {
+                self.data.insert(key.clone(), value.clone());
+            }
+            Command::Delete { key } => {
+                self.data.remove(key);
+            }
+            Command::NoOp => {}
+        }
+        self.last_applied = index;
+    }
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.data.get(key)
+    }
+}
