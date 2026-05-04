33c97dc839ab5a415c5c7df4f9e8bcf2524d13e4
BeNikk <bhattnik442@gmail.com>
2026-05-04 10:04:41 +0530

    feat: add log entry retrieval methods to RaftNode

--- src/raft/log.rs
+++ src/raft/log.rs
@@ -1,3 +1,5 @@
+use crate::raft::RaftNode;
+
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum Command {
     Set { key: String, value: String },
@@ -10,4 +12,19 @@ pub struct LogEntry {
     pub term: u64,        // era of leadership
     pub index: u64,       // position of the command
     pub command: Command, // command which will run
+}
+
+impl RaftNode {
+    pub fn last_log_index(&self) -> u64 {
+        self.persistent.log.len() as u64
+    }
+    pub fn last_log_term(&self) -> u64 {
+        self.persistent.log.last().map(|e| e.term).unwrap_or(0)
+    }
+    pub fn get_entry(&self, index: u64) -> Option<&LogEntry> {
+        if index == 0 {
+            return None;
+        }
+        self.persistent.log.get((index - 1) as usize)
+    }
 }
