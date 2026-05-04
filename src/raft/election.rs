b60476610b019a1e14392269497b5abc4cc48d7e
BeNikk <bhattnik442@gmail.com>
2026-05-04 15:26:31 +0530

    feat: add on_election_timeout method to RaftNode for handling election timeouts and transitioning to candidate state

--- src/raft/election.rs
+++ src/raft/election.rs
@@ -1,10 +1,33 @@
 use crate::raft::{RaftNode, state::*};
 
 impl RaftNode {
+    // this function is called when there is a new term/leader, then our present node becomes a follower.
     pub fn become_follower(&mut self, term: u64) {
         self.persistent.current_term = term;
         self.persistent.voted_for = None;
         self.role = NodeRole::Follower;
         self.votes_received.clear();
+    }
+
+    // this function is called when timeout occurs, it will increment the current term and become a candidate.
+    pub fn on_election_timeout(&mut self) -> Vec<Message> {
+        self.role = NodeRole::Candidate;
+        self.persistent.current_term += 1;
+        self.persistent.voted_for = Some(self.id);
+        self.votes_received.clear();
+        self.votes_received.insert(self.id);
+
+        self.peers
+            .iter()
+            .map(|&peer| Message::RequestVote {
+                to: peer,
+                args: VoteRequest {
+                    term: self.persistent.current_term,
+                    candidate_id: self.id,
+                    last_log_index: self.last_log_index(),
+                    last_log_term: self.last_log_term(),
+                },
+            })
+            .collect()
     }
 }
