becf5b0295dc6f38105c738374f23cba886e40e2
BeNikk <bhattnik442@gmail.com>
2026-05-04 09:37:56 +0530

    feat: implement PersistentState struct for Raft node state management

--- src/raft/state.rs
+++ src/raft/state.rs
@@ -7,3 +7,16 @@ pub enum NodeRole {
     Leader,
 } // A Node can be either a Leader, or a Candidate to be a leader, or a Follower. 
 
+// this is for persisting state of a node, in case of a failure, we can restart from here. 
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct PersistentState {
+    pub current_term: u64,
+    pub voted_for:    Option<NodeId>,
+    pub log:          Vec<LogEntry>,
+}
+
+impl PersistentState {
+    pub fn new() -> Self {
+        Self { current_term: 0, voted_for: None, log: vec![] }
+    }
+}
