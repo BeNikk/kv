a803e78fc0e134e23ddc21f2c4a0e186b48ddc7c
BeNikk <bhattnik442@gmail.com>
2026-05-04 15:07:33 +0530

    feat: add Raft RPC client and server definitions with proto integration

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,1 +1,7 @@
 
+pub mod proto {
+    tonic::include_proto!("raft");
+}
+pub use proto::raft_rpc_client::RaftRpcClient;
+pub use proto::raft_rpc_server::{RaftRpc, RaftRpcServer};
+pub use proto::*;
